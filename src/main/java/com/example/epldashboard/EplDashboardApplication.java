62996c281d21635a0cf1a3392042540eb0adc772
Sharaf <mohammed.sharafeddeen@gmail.com>
2022-12-03 14:13:36 +0200

    Initialize Project

---
+++ src/main/java/com/example/epldashboard/EplDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.example.epldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplDashboardApplication {
+
+    public static void main(String[] args) {
+//        System.exit(SpringApplication.exit(SpringApplication.run(EplDashboardApplication.class, args)));
+        SpringApplication.run(EplDashboardApplication.class, args);
+    }
+
+}
